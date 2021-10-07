9501daeffc3ad0b5766f892af1c2fe6dba257122
N <nastiy1@live.ru>
2021-10-07 22:11:39 +0700

    Задача№2. Реализован интерфейс без описания методов. Методы в процессе

---
+++ Lesson2/Node.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+public class Node:ILinkedList
+{
+	public int Value { get; set; }
+	public Node NextNode { get; set; }
+	public Node PrevNode { get; set; }
+
+
+	public Node()
+	{
+
+
+	}
+
+    public int GetCount()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddNode(int value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddNodeAfter(Node node, int value)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveNode(int index)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RemoveNode(Node node)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Node FindNode(int searchValue)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public interface ILinkedList 
+{
+	int GetCount(); // возвращает количество элементов в списке
+	void AddNode(int value);  // добавляет новый элемент списка
+	void AddNodeAfter(Node node, int value); // добавляет новый элемент списка после определённого элемента
+	void RemoveNode(int index); // удаляет элемент по порядковому номеру
+	void RemoveNode(Node node); // удаляет указанный элемент
+	Node FindNode(int searchValue); // ищет элемент по его значению
+ 
+}
+
